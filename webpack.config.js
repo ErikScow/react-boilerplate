63acee5be2face8ac00afda8c0bbf6c9dbd4f625
Erik Scow <erikscow@gmail.com>
2021-05-19 15:44:19 -0700

    wip-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js,",
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    alias: { react: path.join(__dirname, "node_modules", "react") },
+    extensions: ["*", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      inject: true,
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
